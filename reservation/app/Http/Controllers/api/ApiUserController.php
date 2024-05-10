51976af6bc3c25952d79ad3b7de911e944de3608
codarscoder <codarscoder@gmail.com>
2024-05-10 23:12:52 +0300

    edt

--- reservation/app/Http/Controllers/api/ApiUserController.php
+++ reservation/app/Http/Controllers/api/ApiUserController.php
@@ -7,33 +7,36 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class ApiUserController extends Controller
 {
 
+
     public function login(Request $request)
     {
-
-        $credentials = $request->validate([
+        $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required',
         ]);
 
+        if ($validator->fails()) {
+            return response()->json(['status' => 401, 'message' => $validator->errors()->first()]);
+        }
 
-        // E-posta adresine ait kullanıcıyı alalım
+        // Kullanıcıyı e-posta adresine göre bul
         $user = User::where('email', $request->email)->first();
 
-        if (!$user || !Hash::check($request->password, $user->password)) {
-            return response()->json(['status' => 401]);
-        }
-        // if (Auth::attempt($credentials) ) {
-        if (Auth::loginUsingId($user->id)) {
-            $token = $user->createToken('Login')->accessToken;
-            return response()->json(['token' => $token,'status' => 200]);
+        // Kullanıcıyı doğrulamak için Auth  kullan
+        if (!$user || !Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return response()->json(['status' => 401, 'message' => 'Invalid email or password']);
         }
 
-
+        // Oturum açma başarılıysa, bir token oluştur ve döndür
+        $token = $user->createToken('Login')->accessToken;
+        return response()->json(['token' => $token, 'status' => 200]);
     }
+
 
     /**
      * Store a newly created resource in storage.
