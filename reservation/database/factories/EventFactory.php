1f2be238b2212486d85129757b4badb5164730de
codarscoder <codarscoder@gmail.com>
2024-05-11 15:10:38 +0300

    edt

--- reservation/database/factories/EventFactory.php
+++ reservation/database/factories/EventFactory.php
@@ -1,7 +1,9 @@
 <?php
 namespace Database\Factories;
+
 use App\Models\Event;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Carbon\Carbon;
 
 class EventFactory extends Factory
 {
@@ -9,13 +11,21 @@ class EventFactory extends Factory
 
     public function definition()
     {
+        $baseDate = Carbon::now()->addDays(5); // Mevcut tarihe 5 gün ekleniyor
+
+        $date = $baseDate->toDateString(); // Tarih olarak alınıyor
+        $time = $this->faker->randomElement(['20:00', '21:00', '22:00']); // Rastgele bir saat seçiliyor
+
+        // Tarih ve saat birleştirilerek Carbon nesnesi oluşturuluyor
+        $expireDateTime = Carbon::createFromFormat('Y-m-d H:i', $date . ' ' . $time);
+
         return [
             'user_id' => \App\Models\User::factory(),
             'name' => $this->faker->sentence(),
             'description' => $this->faker->paragraph(),
-            'date' => '2024-08-15',
-            'time' => $this->faker->randomElement(['20:00', '21:00', '22:00']),
-            'expire_at' => null,
+            'date' => $date,
+            'time' => $time,
+            'expire_at' => $expireDateTime,
         ];
     }
 }
--- reservation/routes/web.php
+++ reservation/routes/web.php
@@ -5,38 +5,50 @@ use App\Http\Controllers\web\RegistrationController;
 use App\Http\Controllers\web\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\web\UseController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+
+
 Route::middleware('auth')->group(function () {
+
     Route::get('/', [EventController::class, 'index'])->name('home');
-    Route::get('/create-event', function () {return view('create_event');})->name('create.event.page');
-    Route::post('/create-event',  [EventController::class, 'create_event'])->name('create.event');
-    Route::get('/edit-event/{id}',  [EventController::class, 'edit_event'])->name('edit.event.page');
-    Route::post('/update-event/{id}',  [EventController::class, 'update_event'])->name('update.event');
-    Route::get('/delete-event/{id}',  [EventController::class, 'delete_event'])->name('delete.event');
+
+    Route::get('/create-event', function () {
+        return view('create_event');
+    })->name('create.event.page');
+
+    Route::post('/create-event', [EventController::class, 'create_event'])->name('create.event');
+
+    Route::get('/edit-event/{id}', [EventController::class, 'edit_event'])->name('edit.event.page');
+
+    Route::post('/update-event/{id}', [EventController::class, 'update_event'])->name('update.event');
+
+    Route::get('/delete-event/{id}', [EventController::class, 'delete_event'])->name('delete.event');
+
     Route::get('/my-events', [EventController::class, 'my_events'])->name('my.events');
+
     Route::get('/all-events', [EventController::class, 'all_events'])->name('events.page');
+
     Route::get('/join-event/{id}', [RegistrationController::class, 'join_event'])->name('join.event');
+
     Route::get('/unjoin-event/{id}', [RegistrationController::class, 'unjoin_event'])->name('unjoin.event');
 
 });
 
 
 
-// Route::get('/', [index::class, 'index'])->name('home');
-// Route::get('/about', function () {return view('pages/about'); })->name('about');
 
+// Auth pages
 Route::post('/login', [UserController::class, 'login'])->name('login');
-Route::get('/login', function () {return view('login'); })->name('login.page');
-Route::get('/register', function () {return view('register'); })->name('register.page');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login.page');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register.page');
+
 Route::post('/register', [UserController::class, 'register'])->name('register');
+
 Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 
